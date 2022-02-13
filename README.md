e1c6e69f48629a8249865fe63e8a3813ac349059
Johnny <JohnnyLieu@yahoo.com>
2022-02-13 14:14:02 -0800

    created readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Snake in python
+
+This is the Snake game, similar to the one on the older Nokia phones.
+
+## Video Demo
+<https://www.youtube.com/watch?v=N0ZMSUPn3SQ>
--- app.py
+++ app.py
@@ -14,7 +14,7 @@ orange = (255, 165, 0)
 width, height = 600, 400
 
 game_display = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Snake")
+pygame.display.set_caption("Snake - by Johnny Lieu")
 
 clock = pygame.time.Clock()
 
